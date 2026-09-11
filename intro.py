7988cfbb965668867f02a95dabfff33383fba10e
saminjoroge007-commits <saminjoroge007@gmail.com>
2026-09-11 13:27:59 -0300

    beginning

---
+++ intro.py
@@ -0,0 +1,6 @@
+print("Samuel Njoroge")
+
+print("this is my first ppython code")
+
+total_amount = 1000+340
+print("total_amount")
---
+++ variables.py
@@ -0,0 +1,10 @@
+#they contain the values in them and can be used to store data e.g. total_amount = 2000
+
+# calculate the area of a rectangle
+
+width =  20
+length = 40
+area = length*width
+print("The area of the rectangle is:", area)
+
+
